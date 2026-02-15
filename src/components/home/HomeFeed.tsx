7f00d8a6c98271d0b8161539da08ff98a30c41b7
Jack Nelson <jacknelson@Jacks-Mac-mini.local>
2026-02-15 17:40:26 -0600

    match digest card top padding with sidebar

--- src/components/home/HomeFeed.tsx
+++ src/components/home/HomeFeed.tsx
@@ -99,8 +99,8 @@ export default function HomeFeed() {
       <div className="mx-auto max-w-[1100px] grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-0 lg:gap-6 px-0 lg:px-4 overflow-hidden">
         {/* Center feed */}
         <div className="min-w-0 overflow-hidden max-w-full lg:max-w-[650px] mx-auto lg:mx-0 lg:justify-self-center">
-          <XFeed />
           <DailyDigest />
+          <XFeed />
         </div>
 
         {/* Right sidebar - desktop only */}
