549fb4a9c57f97b6c817756cdf6546ef26f5831a
Jack Nelson <jacknelson@Jacks-Mac-mini.local>
2026-02-15 18:04:55 -0600

    reduce sidebar top padding

--- src/components/home/RightSidebar.tsx
+++ src/components/home/RightSidebar.tsx
@@ -145,7 +145,7 @@ export default function RightSidebar({ mobilePrice = false }: { mobilePrice?: bo
   }
 
   return (
-    <div className="py-3 px-5">
+    <div className="pt-1 pb-3 px-5">
       {/* 1. Combined: Price + Globe + Chart */}
       <MiniPreviewCard />
 
