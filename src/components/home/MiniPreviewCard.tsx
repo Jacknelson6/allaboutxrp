9b9e0eec005239a8a155fa2d72a8ffa761c555b6
Jack Nelson <jacknelson@Jacks-Mac-mini.local>
2026-02-16 18:47:38 -0600

    Fix TradingView widget: use autosize instead of fixed 280px width

--- src/components/home/MiniPreviewCard.tsx
+++ src/components/home/MiniPreviewCard.tsx
@@ -32,13 +32,13 @@ export default function MiniPreviewCard() {
     script.type = 'text/javascript';
     script.innerHTML = JSON.stringify({
       symbol: 'BITSTAMP:XRPUSD',
-      width: 280,
-      height: 240,
+      width: '100%',
+      height: '100%',
       locale: 'en',
       dateRange: '1D',
       colorTheme: 'dark',
       isTransparent: true,
-      autosize: false,
+      autosize: true,
       largeChartUrl: '',
       noTimeScale: false,
       chartOnly: false,
